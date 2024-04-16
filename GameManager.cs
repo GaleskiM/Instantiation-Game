f08e5fe701c5de40312e9c26411371f78670bb34
GaleskiM <sweet10199@yahoo.com>
2024-04-15 23:21:47 -0400

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    [SerializeField] private Player player;
+    [SerializeField] private TextMeshProUGUI scoreUI;
+    [SerializeField] private GameObject playButton;
+    [SerializeField] private GameObject gameOver;
+    
+
+    private int score;
+    public int Score => score;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            DestroyImmediate(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            Application.targetFrameRate = 60;
+            DontDestroyOnLoad(gameObject);
+            Pause();
+        }
+    }
+
+    public void Play()
+    {
+        score = 0;
+        scoreUI.text = score.ToString();
+
+        playButton.SetActive(false);
+        gameOver.SetActive(false);
+
+        Time.timeScale = 1f;
+        player.enabled = true;
+
+        Pipes[] pipes = FindObjectsOfType<Pipes>();
+
+        for (int i = 0; i < pipes.Length; i++)
+        {
+            Destroy(pipes[i].gameObject);
+        }
+    }
+
+    public void GameOver()
+    {
+        playButton.SetActive(true);
+        gameOver.SetActive(true);
+
+        Pause();
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        player.enabled = false;
+    }
+
+    public void IncreaseScore()
+    {
+        score++;
+        scoreUI.text = score.ToString();
+    }
+
+}
